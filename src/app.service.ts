1412b65bb46dca5d414f31f5b1370b86b49c5d96
Sunay Gandhi <sunay@pristinefire.com>
2021-05-02 14:26:09 +0530

    #fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,34 +12,33 @@ export class AppService {
   constructor(private readonly mailerService: MailerService) {}
 
   @Cron(CronExpression.EVERY_MINUTE)
-  getAppointments() {
+  async getSessions() {
     const date = moment()
       .add(1, 'day')
       .format('DD-MM-YYYY');
-    fetch(
-      `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${this.district}&date=${date}`,
-    )
-      .then(
-        res => res.text(),
-        error => Logger.error('error fetching data', error),
-      )
-      .then(body => {
-        const sessions = JSON.parse(body).sessions;
-        const openSessions: any[] = sessions.filter(
-          i => i.min_age_limit < 45 && i.available_capacity > 0,
-        );
-        this.logger.log(`${sessions.length} sessions fetched and parsed`);
-        if (openSessions.length > 0) {
-          const emailBody = openSessions.map(i => {
-            return `${i.name}-${i.available_capacity} doses`;
-          });
-          this.mailerService.sendMail({
-            to: ['sunaygandhi@gmail.com'],
-            subject: `Vaccination Slots on ${date}`,
-            text: emailBody.toString(),
-          });
-          this.logger.log(`found ${openSessions.length} open sessions`);
-        }
-      });
+    try {
+      const res = await fetch(
+        `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${this.district}&date=${date}`
+      );
+      const body = await res.text();
+      const sessions = JSON.parse(body).sessions;
+      const openSessions: any[] = sessions.filter(
+        i => i.min_age_limit < 45 && i.available_capacity > 0,
+      );
+      this.logger.log(`${sessions.length} sessions fetched and parsed`);
+      if (openSessions.length > 0) {
+        const emailBody = openSessions.map(i => {
+          return `${i.name}-${i.available_capacity} doses`;
+        });
+        this.mailerService.sendMail({
+          to: ['sunaygandhi@gmail.com'],
+          subject: `Vaccination Slots on ${date}`,
+          text: emailBody.toString(),
+        });
+        this.logger.log(`found ${openSessions.length} open sessions`);
+      }
+    } catch (e) {
+      Logger.error(e);
+    }
   }
 }
